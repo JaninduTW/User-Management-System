ecac341c5ee36c944ef601d46c3f482e5049ca20
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-08-24 14:53:45 +0530

    Change homepage path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
    <Routes>
 
-    <Route exact path  = "/" element= {<Home/>} />
+    <Route exact path  = "/User-Management-System" element= {<Home/>} />
     <Route exact path  = "/adduser" element= {<AddUser/>} />
     <Route exact path  = "/edituser/:id" element= {<EditUser/>} />
     <Route exact path  = "/viewuser/:id" element= {<ViewUser/>} />
